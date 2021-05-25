f748f3afea9a0d2a6ac1320d1ca5f706766610ec
wacefEssaies96 <57143245+wacefEssaies96@users.noreply.github.com>
2021-05-25 23:17:49 +0100

    changed.env

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,4 +1,4 @@
-{{-- <script src="{{asset('scripts/serviceWorker.js')}}"></script> --}}
+<script src="{{asset('scripts/serviceWorker.js')}}"></script>
 <script src="{{asset('js/app.js')}}"></script>
 <script src="{{asset('dist/js/adminlte.js')}}"></script>
 
